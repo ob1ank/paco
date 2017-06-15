db2faf399e210e59eac75165e541774f826e978a
an15m <568975235@qq.com>
2017-06-15 20:16:14 +0800

    save

--- build.sh
+++ build.sh
@@ -22,10 +22,10 @@ SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
 
 CLI_PATH=$BMV2_PATH/tools/runtime_CLI.py
 
-#$P4C_BM_SCRIPT p4src/tag.p4 --json tag.json
+$P4C_BM_SCRIPT p4src/tag.p4 --json tag.json
 # This gives libtool the opportunity to "warm-up"
-sudo $SWITCH_PATH >/dev/null 2>&1
-sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python topo.py \
-    --behavioral-exe $SWITCH_PATH \
-    --json tag.json \
-    --cli $CLI_PATH
+#sudo $SWITCH_PATH >/dev/null 2>&1
+#sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python topo.py \
+#    --behavioral-exe $SWITCH_PATH \
+#    --json tag.json \
+#    --cli $CLI_PATH
