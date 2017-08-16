a48125bf2af5905cec2e2eb0a8887d4cc92a80e5
an15m <568975235@qq.com>
2017-08-16 15:44:17 +0800

    save

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 CLI_PATH=/home/snail/apps/behavioral-model/targets/simple_switch/sswitch_CLI
-for i in 1 2 3 4 5 6 7 8 9 10 11
+for i in 1 2 3 4 5
 do
     command_file="commands/commands_s"$i".txt"
     port=`expr 22222 + $i - 1`
-    $CLI_PATH paco.json  $port   < $command_file
+    $CLI_PATH openflow.json  $port   < $command_file
 done
