fd83d830cd61be6cc8d5ca1d19f6ffe51696dc40
an15m <568975235@qq.com>
2017-08-17 16:55:06 +0800

    save

--- commands.sh
+++ commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CLI_PATH=/home/snail/apps/behavioral-model/targets/simple_switch/sswitch_CLI
-for i in 1 2 3 4 5 6 7 8 9 10 11
+for i in 1 2 3 4 5 6 7
 do
     command_file="commands/commands_s"$i".txt"
     port=`expr 22222 + $i - 1`
